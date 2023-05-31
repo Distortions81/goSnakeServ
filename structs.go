383da02e260df342cd9a590ab038267e359731a3
Distortions81 <carlotto81@gmail.com>
2023-05-30 20:10:51 -0600

    .

--- structs.go
+++ structs.go
@@ -1,14 +1,32 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
+
+type lobbyListing struct {
+	Name        string
+	PlayerCount string
+	PlayerNames string
+	Started     string
+	Remaining   string
+	BoardSize   uint8
+	Level       uint16
+}
 
 type lobbyData struct {
 	ID      uint64
 	Name    string
 	Players []playerData
+	Ticks   uint64
+	Started time.Time
+	EndTime time.Time
+	Level   uint16
 	Tiles   [MAX_BOARD_SIZE][MAX_BOARD_SIZE]bool
 
-	Lock sync.Mutex
+	Listing lobbyListing
+	Lock    sync.Mutex
 }
 
 type playerData struct {
