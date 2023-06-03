ce84290de757f4fa00fb05b8dbc0a217afa0cbbd
Distortions81 <carlotto81@gmail.com>
2023-06-02 21:54:31 -0600

    .

--- httpHandler.go
+++ httpHandler.go
@@ -24,32 +24,24 @@ func httpsHandler(w http.ResponseWriter, r *http.Request) {
 	sessionID := makeID()
 	player := playerData{ID: sessionID}
 
-	/* Incoming get? Send to file server */
-	if r.Method != http.MethodPost {
-		/* Anything other than get or post, just silently reject it */
+	cwlog.DoLog(true, "Starting read loop.")
+	/* Read body */
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		cwlog.DoLog(true, "Error reading request body: %v", err)
 		return
 	}
-	cwlog.DoLog(true, "Starting read loop.")
+	input := string(bytes)
 
-	for {
-		/* Read body */
-		bytes, err := io.ReadAll(r.Body)
-		if err != nil {
-			cwlog.DoLog(true, "Error reading request body: %v", err)
-			return
-		}
-		input := string(bytes)
-
-		/* Empty body, silently reject */
-		if input == "" {
-			continue
-		}
-
-		/* Send to command parser */
-		if !commandParser(input, w, &player) {
-			return
-		}
-
-		time.Sleep(readSleep)
+	/* Empty body, silently reject */
+	if input == "" {
+		return
 	}
+
+	/* Send to command parser */
+	if !commandParser(input, w, &player) {
+		return
+	}
+
+	time.Sleep(readSleep)
 }
