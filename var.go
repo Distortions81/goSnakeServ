1a19cd5a0e89b4c9e2160bd11dd239bc2f01d9ca
Distortions81 <carlotto81@gmail.com>
2023-06-04 10:34:24 -0600

    Add test info

--- var.go
+++ var.go
@@ -4,10 +4,18 @@ func init() {
 	players = make(map[uint64]*playerData)
 }
 
+var testPlayerList = []*playerData{
+	{Name: "IUseArchBTW1337hax0rluserftw"},
+	{Name: "SteamDeckUser"},
+	{Name: "SnakeyMcSnakeFace"},
+	{Name: "ITHINKIAMCLEVERANDVERYFUNNYHAHAHAHA12345679ROFLMAOBBQ"},
+}
+
 var lobbyList = []lobbyData{
 	{
-		ID:   0,
-		Name: "Welcome",
+		ID:      0,
+		Name:    "People sometimes use really long names for stuff and things",
+		Players: testPlayerList,
 	},
 }
 
