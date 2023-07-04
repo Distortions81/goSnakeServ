317c9203b43dd185373f7ee94c514fedc436e1e6
Distortions81 <carlotto81@gmail.com>
2023-07-03 18:21:52 -0600

    rewrite start

--- cmdParse.go
+++ cmdParse.go
@@ -12,6 +12,51 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func newParser(data []byte) {
+	dataLen := len(data)
+
+	if dataLen <= 0 {
+		return
+	}
+
+	d := data[0]
+	if d < 0x10 {
+		switch d {
+		case 0x01:
+			//init
+		case 0x02:
+			//ping
+		}
+	} else if d < 0x20 {
+		switch d {
+		case 0x10:
+			//Login
+		case 0x11:
+			//Set Name
+		}
+	} else if d < 0x30 {
+		switch d {
+		case 0x20:
+			//get lobbies
+		case 0x21:
+			//join lobby
+		case 0x22:
+			//create lobby
+		case 0x23:
+			//set lobby settings
+		}
+	} else if d < 0x40 {
+		switch d {
+		case 0x30:
+			//spawn
+		case 0x31:
+			//go dir
+		}
+	} else if d < 0x50 {
+		//Reserved
+	}
+}
+
 func commandParser(input string, c *websocket.Conn) {
 
 	/* Before ID check */
